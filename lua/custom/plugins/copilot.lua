6a796df4e4c98759987a9e9d99bbb1a82656f320
charlie <astromech12000@gmail.com>
2023-07-29 04:43:36 +0900

    Changed copilot config

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,17 +4,28 @@ return {
     cmd = 'Copilot',
     event = 'InsertEnter',
     config = function ()
-      require('copilot').setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false }
-      }
+      require('copilot').setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<Tab>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      })
     end
   },
-  {
-    'zbirenbaum/copilot-cmp',
-    dependencies = { 'zbirenbaum/copilot.lua' },
-    config = function ()
-      require('copilot_cmp').setup()
-    end
-  }
+  -- {
+  --   'zbirenbaum/copilot-cmp',
+  --   dependencies = { 'zbirenbaum/copilot.lua' },
+  --   config = function ()
+  --     require('copilot_cmp').setup()
+  --   end
+  -- }
 }
